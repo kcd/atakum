d0b7e52fd7e35db34d4516e5a6376bb572a0aca1
nurdan tuğçe yeşilyurt <tugce.yesilyurt@bil.omu.edu.tr>
2013-04-13 17:23:21 +0300

    .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atakum::Application.config.secret_token = 'a5e9ae7fe9ca4e3679277224aff52575e12e4b72890718b44b957cd594f04da963c7f6b0223d6f2810cca913b8a01b116c7d81eda3abf5594661953e143361c5'
